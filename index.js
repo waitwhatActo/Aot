8d59beab835c9649559a58160917bbe335fa96fd
cleverActon0126 <59557379+cleverActon0126@users.noreply.github.com>
2020-02-11 16:27:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const Discord = require("discord.js");
+const YTDL = require("ytdl-core");
 
 const TOKEN = "NjU1NzY5Njk1MzcwMjE1NDI1.XjzbnQ.1RBMHcmr-pDIYGoGpNQRDwhTPuE";
 const PREFIX = "?a";
+
+function play(connection, message) {
+  var servers = servers[message.guild.id]
+
+  server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+
+  server.queue.shift();
+
+  server.dispatcher.on("end", function() {
+    if(server.queue[0]) play (connection, message);
+    else connection.disconnect();
+  });
+}
 
 var fortunes = [
   "🟢It is decidedly so.",
@@ -107,15 +121,15 @@ bot.on("message", function(message) {
     case "update":
     var embed = new Discord.RichEmbed()
     .setTitle("Update Successful!")
-    .setDescription("Successfully updated to Version 0.8.0!")
+    .setDescription("Successfully updated to Version 0.9.0!")
     .addField("Prefix", "?a \(Uncustomable\)")
-    .addField("Commands", "`help` \(Will lead you to other help commands\), `hello`, `aot`, `bye`, `noticeme`, `support`, `salmon`, `apple`, `pie`, `candy`, `mess`, `messer`, `messest`, `8ball`, `ding`, `ping`, `beep` " )
-    .addField("New Commands", "N/A", true)
+    .addField("Commands", "`help` \(Will lead you to other help commands\), `hello`, `aot`, `bye`, `noticeme`, `support`, `salmon`, `apple`, `pie`, `candy`, `mess`, `messer`, `messest`, `8ball`, `ding`, `ping`, `beep` , `play {song name}` , `stop` , `skip`" )
+    .addField("New Commands", "`play {song name}`, `skip`, `stop`", true)
     .addField("Removed Commands", "N/A", true)
-    .addField("Updates", "Bot will give you roles when you join. (Trying to get Mee6 to do nothing!)")
+    .addField("Updates", "Bot can now play music! (Rythm will get kicked.)")
     .setColor(0x00ff00)
     .setTimestamp()
-    .setFooter("Aot Version 0.8.0, Made by cleverActon0126#3517")
+    .setFooter("Aot Version 0.9.0, Made by cleverActon0126#3517")
     message.channel.send(embed);
     break;
     case "help":
@@ -127,7 +141,7 @@ bot.on("message", function(message) {
     .addField("😀Fun Menu😀", "`helpfun`", true)
     .setColor(0x00ffff)
     .setTimestamp()
-    .setFooter("Aot Version 0.8.0, Made by cleverActon0126#3517")
+    .setFooter("Aot Version 0.9.0, Made by cleverActon0126#3517")
     message.channel.send(embed);
     break;
     case "helpgeneral":
@@ -140,7 +154,7 @@ bot.on("message", function(message) {
     .addField("`support`", "To report a bug and have some Aot support", true)
     .setColor(0x00ffff)
     .setTimestamp()
-    .setFooter("Aot Version 0.8.0, Made by cleverActon0126#3517")
+    .setFooter("Aot Version 0.9.0, Made by cleverActon0126#3517")
     message.channel.send(embed);
     break;
     case "helpfood":
@@ -152,7 +166,7 @@ bot.on("message", function(message) {
     .addField("`candy`", "Sweet one", true)
     .setColor(0x00ffff)
     .setTimestamp()
-    .setFooter("Aot Version 0.8.0, Made by cleverActon0126#3517")
+    .setFooter("Aot Version 0.9.0, Made by cleverActon0126#3517")
     message.channel.send(embed);
 
     break;
@@ -164,7 +178,7 @@ bot.on("message", function(message) {
     .addField("`messest`", "??? mess up", true)
     .setColor(0x00ffff)
     .setTimestamp()
-    .setFooter("Aot Version 0.8.0, Made by cleverActon0126#3517")
+    .setFooter("Aot Version 0.9.0, Made by cleverActon0126#3517")
     message.channel.send(embed);
     break;
     case "helpfun":
@@ -176,7 +190,7 @@ bot.on("message", function(message) {
     .addField("`beep`", "Beep, beep, boop, boop", true)
     .setColor(0x00ffff)
     .setTimestamp()
-    .setFooter("Aot Version 0.8.0, Made by cleverActon0126#3517")
+    .setFooter("Aot Version 0.9.0, Made by cleverActon0126#3517")
     message.channel.send(embed);
     break;
     case "support":
@@ -185,8 +199,39 @@ bot.on("message", function(message) {
     .addField("Press the link below to have some Aot support or report a bug!", "https://bit.ly/Aot_Support")
     .setColor(0xff0000)
     .setTimestamp()
-    .setFooter("Aot Version 0.8.0, Made by cleverActon0126#3517")
+    .setFooter("Aot Version 0.9.0, Made by cleverActon0126#3517")
     message.channel.send(embed);
+    break;
+    case "play":
+    if(!args[1]) {
+      message.channel.send("Please provide a link.");
+      return;
+    }
+
+    if(!message.member.voiceChannel) {
+      message.channel.send("You must be in a voice channel.");
+      return;
+    }
+
+    if(!servers[message.guild.id] servers[message.guild.id]) = {
+      queue: []
+    };
+
+    var server = servers[message.guild.id];
+
+    if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+      play(connesctio, message);
+    });
+    break;
+    case "skip":
+    var server = servers[message.guild.id];
+
+    if(server.dispatcher) server.dispatcher.end();
+    break;
+    case "stop":
+    var server = servers[message.guild.id];
+
+    if(message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
     break;
     default:
     message.channel.send("**Error 404**:Invalid command!");
