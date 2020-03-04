faf99e8d6cc76495e21fb5c58fad3e83865cc39a
cleverActon0126 <59557379+cleverActon0126@users.noreply.github.com>
2020-03-04 10:54:38 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ var moment = require('moment');
 bot.on("ready", function() {
   console.log("Connected as Aot#0350");
   bot.user.setStatus('online')
-  bot.user.setActivity("Watching", "Acton scripting me!")
+  bot.user.setActivity("Acton scripting me!", "Watching")
 });
 
 bot.on("guildMemberAdd", function(member) {
